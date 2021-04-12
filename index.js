93699d83db70a6c3236545c3c8098094f0f43724
Hitesh Sharma <hitesh.sharma_cs18@gla.ac.in>
2021-04-12 16:46:17 +0530

    index

---
+++ index.js
@@ -0,0 +1,15 @@
+const express=require('express');
+const app=express();
+// app.listen(3000) 3000-port number randomly
+
+// Provide suitable port if available otherwise 4000
+const Port=process.env.PORT || 4000;
+app.listen(Port,()=>{console.log("server connected")})
+
+// app.method(<route>, callback)  methods- get, post, delete, push
+//app.get('/',(req,res));
+app.get('/',(req,res)=>{res.send('hello')});
+
+//node index.js
+//nodemon index.js -- not required to refresh
+//package.json -> script -> "start":"nodemon index.js"
